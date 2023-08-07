2a9fbdfcf995bafc696b79654a808dba45202a91
Raihan Hossain <hossainraihan02@gmail.com>
2023-08-07 13:00:57 +0600

    first version final

--- test/responsive_card_test.dart
+++ test/responsive_card_test.dart
@@ -1,12 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:responsive_card/responsive_card.dart';
+
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    const ResponsiveCard(title: Text("Raihan Hossain"),);
   });
 }
